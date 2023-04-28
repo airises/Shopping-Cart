3b1b5400199553e26721b39f1853c2a31ca6d9e3
airises <isaevasai7@gmail.com>
2023-04-28 21:24:21 +0600

    shopping-cart

--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useReducer } from "react";
+import "./App.css";
+import { jeweliers } from "./utils/constant";
+import List from "./shopping-cart/List";
+import Card from "./shopping-cart/Card";
 
+const initialState = {
+  product: JSON.parse(localStorage.getItem("auth")) || jeweliers,
+};
+const onlineReducer = (state, action) => {
+  switch (action.type) {
+    case "addProduct":
+      return {
+        ...state,
+        product: state.product.map((item) => {
+          if (item.id === action.payload) {
+            return {
+              ...item,
+              quantity: item.quantity + 1,
+            };
+          }
+          return item;
+        }),
+      };
+    case "incrementProduct":
+      return {
+        ...state,
+        product: state.product.map((item) => {
+          if (item.id === action.payload) {
+            return {
+              ...item,
+              quantity: item.quantity + 1,
+              price: item.price + item.staticPrice,
+            };
+          }
+          return item;
+        }),
+      };
+    case "decrementProduct":
+      return {
+        ...state,
+        product: state.product.map((item) => {
+          if (item.id === action.payload && item.quantity !== 1) {
+            return {
+              ...item,
+              quantity: item.quantity - 1,
+              price: item.price - item.staticPrice,
+            };
+          }
+          return item;
+        }),
+      };
+    case "removeProduct":
+        return {
+          ...state,
+          product: state.product.map((item) => {
+            if (item.id === action.payload && item.quantity !== 0) {
+              return {
+                ...item,
+                quantity: (item.quantity = 0),
+                price: item.staticPrice,
+              };
+            }
+            return item;
+          }),
+        };
+    default:
+      return state;
+  }
+};
 function App() {
+  const [store, dispatchStore] = useReducer(onlineReducer, initialState);
+
+  useEffect(() => {
+    localStorage.setItem("auth", JSON.stringify(store.product));
+  }, [store.product]);
+
+  const incrementProductHandler = (id) => {
+    dispatchStore({ type: "incrementProduct", payload: id });
+  };
+  const decrementProductHandler = (id) => {
+    dispatchStore({ type: "decrementProduct", payload: id });
+  };
+  const addProductHandler = (id) => {
+    dispatchStore({ type: "addProduct", payload: id });
+  };
+
+  const removeProd = (id) => {
+    dispatchStore({ type: "removeProduct", payload: id });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Card addProductHandler={addProductHandler} store={store.product} />
+      <List
+        decrementProductHandler={decrementProductHandler}
+        incrementProductHandler={incrementProductHandler}
+        store={store.product}
+        removeProd={removeProd}
+      />
+    </>
   );
 }
-
 export default App;
